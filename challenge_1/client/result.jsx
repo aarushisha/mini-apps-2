2d19d333a206c8b37f0149f3b01f3be5d712d515
aarushis.s.90@gmail.com <aarushis.s.s90@gmail.com>
2019-07-02 10:47:59 -0700

    editable table

--- challenge_1/client/result.jsx
+++ challenge_1/client/result.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 const Result = (props) => {
   return (
       <tr>
-        <td><button>Edit</button></td>
         <td><button>Save</button></td>
-      <td className="date">{props.date}</td>
-      <td className="description">{props.description}</td>
+      <td className="date" contentEditable={true}>{props.date}</td>
+      <td className="description" contentEditable={true}>{props.description}</td>
       </tr>
   )
 }
