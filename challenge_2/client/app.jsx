76ae617ff95597c1246e1c435faa572d4dce2725
aarushis.s.90@gmail.com <aarushis.s.s90@gmail.com>
2019-07-02 11:24:05 -0700

    initial setup

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,19 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>Cryptocurrency Charting Tool</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const port = 3004;
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log('listening at port ', port);
+})
 
