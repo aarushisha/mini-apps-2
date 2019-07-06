e02af9b5f50732c5a14cde629253e36035ce99dd
aarushis.s.90@gmail.com <aarushis.s.s90@gmail.com>
2019-07-06 13:00:26 -0700

    initial webpack setup complete

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h3>Bowling!</h3>
+      </div>
+    )
+  }
+}
 
+ReactDom.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8008;
 
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.listen(port, () => console.log('listening on port', port))
